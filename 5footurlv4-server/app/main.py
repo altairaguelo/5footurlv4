4caec47ac33965b000997dc8d41af2fb0bfaeb93
alaguelo <alaguelo@ucsd.edu>
2024-09-05 19:59:44 -0700

    absolute pathing try again

--- 5footurlv4-server/app/main.py
+++ 5footurlv4-server/app/main.py
@@ -4,7 +4,7 @@ from firebase_admin import db
 import os
 from firebase_admin import credentials
 #empty
-cred_path = os.path.join(os.path.dirname(__file__), 'ServiceAccountKey.json')
+cred_path = os.path.join(os.oath.dirname(os.path.dirname(__file__)), 'ServiceAccountKey.json')
 cred_obj = credentials.Certificate(cred_path)
 default_app = firebase_admin.initialize_app(cred_obj, {
     'databaseURL': 'https://footurlv4-default-rtdb.firebaseio.com/'
