7f8224c086c46524a5623a41ae6b52411baba5c2
alaguelo <alaguelo@ucsd.edu>
2024-09-06 15:44:12 -0700

    removed browserroute thing new build

--- 5footurlv4/src/App.js
+++ 5footurlv4/src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Form from "./components/Form";
 
@@ -9,12 +9,10 @@ function App() {
     <div className="App">
       <div className='auth-wrapper'>
         <div className='auth-inner'>
-          <BrowserRouter basename='/app'>
             <Routes>
               <Route exact path='/' element={<Form />} />
               <Route path="/app" element={<Form />} />
             </Routes>
-          </BrowserRouter>
         </div>
       </div>
     </div>
